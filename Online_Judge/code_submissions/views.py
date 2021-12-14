ac3ff0fa40149d88a07db9ebed57b458ca3812a1
Shale Arkane <shalearkane@gmail.com>
2021-12-14 15:16:47 +0530

    it works

--- Online_Judge/code_submissions/views.py
+++ Online_Judge/code_submissions/views.py
@@ -8,20 +8,18 @@ with open(settings.BASE_DIR + '/realtimedbcreds.json') as f:
     config = json.load(f)
 
 firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+authe = firebase.auth()
 database = firebase.database()
 
 
 def index(request):
     # accessing our firebase data and storing it in a variable
-    name = database.child('Data').child('Name').get().val()
-    stack = database.child('Data').child('Stack').get().val()
-    framework = database.child('Data').child('Framework').get().val()
+    name = database.child('Data').child('another').get().val()
+    stack = database.child('Data').child('something').get().val()
 
     context = {
         'name': name,
         'stack': stack,
-        'framework': framework
     }
     print(context)
     return render(request, 'index.html', context)
